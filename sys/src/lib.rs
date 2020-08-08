51a53c82a80c3094f4fd5059b2567e7a5b21ed69
miroox <miroox@outlook.com>
2020-08-08 13:51:18 +0800

    wll-sys document

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -2,4 +2,10 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+//! # wll-sys
+//!
+//! `wll-sys` is a low-level bindings for Wolfram Library.
+//!
+//! **see also**: [Wolfram LibraryLink User Guide](http://reference.wolfram.com/language/LibraryLink/tutorial/Overview.html).
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
