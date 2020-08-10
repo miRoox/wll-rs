7eafc0ad520b709fe398cad6055baf2f93365f8e
miroox <miroox@outlook.com>
2020-08-10 21:05:59 +0800

    num_traits document

--- src/num_traits.rs
+++ src/num_traits.rs
@@ -1,17 +1,25 @@
 use std::convert::{TryFrom, TryInto};
 
+/// The basic trait for numeric types.
 pub trait Number: Clone + PartialEq {
+    /// Returns the zero element of `Self`.
     fn zero() -> Self;
+    /// Returns the identity element of `Self`.
     fn one() -> Self;
 }
 
+/// The basic trait for real numbers.
 pub trait RealNumber: Number {}
 
+/// Compatible trait for `wll_sys::mcomplex`.
 pub trait ComplexType: Number + TryFrom<wll_sys::mcomplex> + TryInto<wll_sys::mcomplex> {}
 
+/// Compatible trait for `wll_sys::mreal`.
 pub trait RealType: RealNumber + Into<wll_sys::mreal> {
     fn from(num: wll_sys::mreal) -> Self;
 }
+
+/// Compatible trait for `wll_sys::mint`.
 pub trait IntegerType: RealNumber + TryFrom<wll_sys::mint> + TryInto<wll_sys::mint> {}
 
 macro_rules! impl_number_trait {
