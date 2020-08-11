e01a5dee1d86445596a3acac689e92cfa8392e30
miroox <miroox@outlook.com>
2020-08-11 12:10:44 +0800

    call the setup function with wll_setup

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,11 +2,23 @@ extern crate proc_macro;
 
 use crate::proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, ItemFn, ReturnType, Signature};
 
 #[proc_macro_attribute]
 pub fn wll_setup(_args: TokenStream, input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as ItemFn);
+    let funcall = match &ast.sig {
+        Signature {
+            ident: id,
+            inputs: params,
+            asyncness: None,
+            unsafety: None,
+            variadic: None,
+            output: ReturnType::Default,
+            ..
+        } if params.is_empty() => quote! { #id() },
+        _ => panic!("Invalid function signature!"),
+    };
     (quote! {
         #ast
         #[no_mangle]
@@ -16,6 +28,7 @@ pub fn wll_setup(_args: TokenStream, input: TokenStream) -> TokenStream {
             if let Err(e) = ::wll::global::initialize_lib_data(data) {
                 e.to_raw_error()
             } else {
+                #funcall;
                 ::wll_sys::LIBRARY_NO_ERROR
             }
         }
