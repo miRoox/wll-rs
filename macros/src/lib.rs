149237304c845cc3cafeb93535d72fe340d32d64
miroox <miroox@outlook.com>
2020-08-21 20:25:00 +0800

    finish wll_export

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -7,7 +7,10 @@ extern crate proc_macro;
 use crate::proc_macro::TokenStream;
 use quote::{format_ident, quote, quote_spanned, ToTokens};
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, AttributeArgs, ItemFn, Lit, Meta, NestedMeta, ReturnType, Signature};
+use syn::{
+    parse_macro_input, AttributeArgs, FnArg, ItemFn, Lit, LitInt, Meta, NestedMeta, ReturnType,
+    Signature,
+};
 
 /// Mark the function as the initialization function for Wolfram LibraryLink.
 ///
@@ -35,7 +38,7 @@ pub fn wll_setup(_args: TokenStream, input: TokenStream) -> TokenStream {
                 quote_spanned! { id.span() => #id() }
             }
         }
-        sig => syn::Error::new(sig.span(), "Invalid function signature!").to_compile_error(),
+        sig => invalid_function_signature(sig.span()),
     };
     (quote! {
         #[inline(always)]
@@ -75,7 +78,7 @@ pub fn wll_teardown(_args: TokenStream, input: TokenStream) -> TokenStream {
             output: ReturnType::Default,
             ..
         } if params.is_empty() => quote_spanned! { id.span() => #id() },
-        sig => syn::Error::new(sig.span(), "Invalid function signature!").to_compile_error(),
+        sig => invalid_function_signature(sig.span()),
     };
     (quote! {
         #[inline(always)]
@@ -94,10 +97,52 @@ pub fn wll_teardown(_args: TokenStream, input: TokenStream) -> TokenStream {
 pub fn wll_export(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as AttributeArgs);
     let input = parse_macro_input!(input as ItemFn);
-    let Signature { ident: funame, .. } = &input.sig;
+    let Signature {
+        ident: funame,
+        inputs: params,
+        paren_token: sig_paren,
+        ..
+    } = &input.sig;
     let exportname = match args.len() {
         0 => format_ident!("wll_{}", funame).into_token_stream(),
         _ => get_export_name_from_meta(&args[0]),
+    };
+    let paramc = params.len();
+    let paramclit = LitInt::new(&paramc.to_string(), sig_paren.span);
+    let argvars = (0..paramc).map(|i| format_ident!("arg{:o}", i));
+    let argvars2 = argvars.clone();
+    let arggets = params.iter().enumerate().map(|(i, arg)| match arg {
+        FnArg::Receiver(recr) => invalid_function_signature(recr.span()),
+        FnArg::Typed(pty) => {
+            let ty = &pty.ty;
+            let inner = quote_spanned! { ty.span() =>
+                <#ty>::try_get_arg(args.add(#i).read())
+            };
+            unwrap_errcode(inner, ty.span())
+        }
+    });
+    let (funcall, setres) = match &input.sig {
+        Signature {
+            asyncness: None,
+            variadic: None,
+            output: ReturnType::Type(_, ty),
+            ..
+        } => {
+            let inner = quote_spanned! { input.sig.span() =>
+                #funame(#(#argvars2),*)
+            };
+            let setres = quote_spanned! { ty.span() =>
+                match ret.try_set_arg(&res) {
+                    ::std::result::Result::Ok(()) => ::wll_sys::LIBRARY_NO_ERROR,
+                    ::std::result::Result::Err(err) => err.to_raw_error(),
+                }
+            };
+            (unwrap_errcode(inner, ty.span()), setres)
+        }
+        sig => (
+            invalid_function_signature(sig.span()),
+            invalid_function_signature(sig.span()),
+        ),
     };
     (quote! {
         #[inline]
@@ -109,7 +154,14 @@ pub fn wll_export(args: TokenStream, input: TokenStream) -> TokenStream {
             args: *const ::wll_sys::MArgument,
             res: ::wll_sys::MArgument,
         ) -> ::wll_sys::errcode_t {
-            // TODO
+            use ::wll::adaptor::{MArgumentGetter, MArgumentSetter};
+            let _lib_data = ::wll::global::LibDataLocalizer::new(lib_data);
+            if argc != #paramclit {
+                return ::wll_sys::LIBRARY_TYPE_ERROR;
+            }
+            #(let #argvars = #arggets;)*
+            let ret = #funcall;
+            #setres
         }
     })
     .into()
@@ -133,4 +185,20 @@ fn get_export_name_from_meta(meta: &NestedMeta) -> proc_macro2::TokenStream {
             syn::Error::new(other.span(), "expected identifier for export name.").to_compile_error()
         }
     }
+}
+
+fn unwrap_errcode(
+    expr: proc_macro2::TokenStream,
+    span: proc_macro2::Span,
+) -> proc_macro2::TokenStream {
+    quote_spanned! { span =>
+        match #expr {
+            ::std::result::Result::Ok(val) => val,
+            ::std::result::Result::Err(err) => return err.to_raw_error(),
+        }
+    }
+}
+
+fn invalid_function_signature(span: proc_macro2::Span) -> proc_macro2::TokenStream {
+    syn::Error::new(span, "invalid function signature!").to_compile_error()
 }
