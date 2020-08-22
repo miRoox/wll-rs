7a0943dfe8ce8868bb2be9e5ce0bd80feaf7dbd8
miroox <miroox@outlook.com>
2020-08-22 20:37:15 +0800

    install LLVM for Windows

---
+++ .github/workflows/intall-llvm.ps1
@@ -0,0 +1,14 @@
+# based on https://www.powershellgallery.com/packages/AppVeyorBYOC/1.0.170/Content/scripts%5CWindows%5Cinstall_llvm.ps1
+
+$llvmVersion = "10.0.0"
+Write-Host "Installing LLVM $llvmVersion ..." -ForegroundColor Cyan
+Write-Host "Downloading..."
+$exePath = "$env:temp\LLVM-$llvmVersion-win64.exe"
+(New-Object Net.WebClient).DownloadFile("https://github.com/llvm/llvm-project/releases/download/llvmorg-$llvmVersion/LLVM-$llvmVersion-win64.exe", $exePath)
+Write-Host "Installing..."
+cmd /c start /wait $exePath /S
+[System.Environment]::SetEnvironmentVariable("LIBCLANG_PATH", "$env:ProgramFiles\LLVM\bin", [System.EnvironmentVariableTarget]::Machine)
+
+cmd /c clang --version
+
+Write-Host "Installed" -ForegroundColor Green
