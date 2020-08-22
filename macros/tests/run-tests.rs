f4cbf9076ee86ff4d47d98084f5c0943e2b6b2fc
miroox <miroox@outlook.com>
2020-08-22 15:28:29 +0800

    move ui test files to inner folder

--- macros/tests/run-tests.rs
+++ macros/tests/run-tests.rs
@@ -1,18 +1,18 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    t.pass("tests/empty-setup.rs");
-    t.pass("tests/setup-return.rs");
-    t.pass("tests/add-two.rs");
-    t.pass("tests/factorial.rs");
-    t.compile_fail("tests/conflict-name.rs");
-    t.pass("tests/complex-conj.rs");
-    t.compile_fail("tests/complex-int-input.rs");
-    t.pass("tests/complex-int-output.rs");
-    t.pass("tests/no-input.rs");
-    t.pass("tests/void-return.rs");
-    t.pass("tests/do-nothing.rs");
-    t.compile_fail("tests/missing-result.rs");
-    t.compile_fail("tests/missing-return.rs");
-    t.pass("tests/redefine-prelude.rs");
+    t.pass("tests/ui/empty-setup.rs");
+    t.pass("tests/ui/setup-return.rs");
+    t.pass("tests/ui/add-two.rs");
+    t.pass("tests/ui/factorial.rs");
+    t.compile_fail("tests/ui/conflict-name.rs");
+    t.pass("tests/ui/complex-conj.rs");
+    t.compile_fail("tests/ui/complex-int-input.rs");
+    t.pass("tests/ui/complex-int-output.rs");
+    t.pass("tests/ui/no-input.rs");
+    t.pass("tests/ui/void-return.rs");
+    t.pass("tests/ui/do-nothing.rs");
+    t.compile_fail("tests/ui/missing-result.rs");
+    t.compile_fail("tests/ui/missing-return.rs");
+    t.pass("tests/ui/redefine-prelude.rs");
 }
