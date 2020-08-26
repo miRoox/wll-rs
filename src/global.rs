491cd5edccb3ed6c72a8405ef6050df8ecf6b35b
miroox <miroox@outlook.com>
2020-08-26 15:26:03 +0800

    try to use thread local lib data

--- src/global.rs
+++ src/global.rs
@@ -1,24 +1,23 @@
 //! Some tools to access the global status.
 
 use crate::errors::Error;
-use std::mem;
+use std::cell::RefCell;
 
-// should be thread local?
-pub(crate) static mut CURRENT_LIB_DATA: Option<sys::WolframLibraryData> = None;
+thread_local!(
+    static CURRENT_LIB_DATA: RefCell<Option<sys::WolframLibraryData>> = RefCell::new(None);
+);
 
 /// initialize global `WolframLibraryData`.
 #[inline]
 pub fn initialize_lib_data(lib_data: Option<sys::WolframLibraryData>) -> Result<(), Error> {
-    unsafe {
-        CURRENT_LIB_DATA = lib_data;
-    }
+    CURRENT_LIB_DATA.with(|data| data.replace(lib_data));
     Ok(())
 }
 
 /// get current `WolframLibraryData`.
 #[inline]
-pub fn get_lib_data() -> &'static Option<sys::WolframLibraryData> {
-    unsafe { &CURRENT_LIB_DATA }
+pub fn get_lib_data() -> Option<sys::WolframLibraryData> {
+    CURRENT_LIB_DATA.with(|data| *data.borrow())
 }
 
 /// RAII wrapper to set current `WolframLibraryData` locally.
@@ -30,19 +29,15 @@ impl LibDataLocalizer {
     /// set current `WolframLibraryData` locally.
     #[inline]
     pub fn new(new: Option<sys::WolframLibraryData>) -> Self {
-        let mut old = new;
-        unsafe {
-            mem::swap(&mut old, &mut CURRENT_LIB_DATA);
+        LibDataLocalizer {
+            old: CURRENT_LIB_DATA.with(|data| data.replace(new)),
         }
-        LibDataLocalizer { old }
     }
 }
 
 impl Drop for LibDataLocalizer {
     #[inline]
     fn drop(&mut self) {
-        unsafe {
-            mem::swap(&mut self.old, &mut CURRENT_LIB_DATA);
-        }
+        CURRENT_LIB_DATA.with(|data| data.replace(self.old));
     }
 }
